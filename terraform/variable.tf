55b4de5a9ee2f07aadfc8f755ef39bbb6868b737
soylucagatay <118979860+soylucagatay@users.noreply.github.com>
2022-11-30 13:53:09 +0300

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -30,5 +30,5 @@ variable "alert_email_id" {
 variable "repo_url" {
   description = "Repository url to clone into production machine"
   type        = string
-  default     = "https://github.com/josephmachado/data_engineering_project_template.git"
+  default     = "https://github.com/soylucagatay/Project_template.git"
 }
